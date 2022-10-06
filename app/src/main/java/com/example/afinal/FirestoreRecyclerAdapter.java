d422e9413271a649fef8899ba6429eb81ec4fed4
pranavbinwar <82322955+pranavbinwar@users.noreply.github.com>
2022-10-06 11:47:59 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/afinal/FirestoreRecyclerAdapter.java
@@ -0,0 +1,6 @@
+package com.example.afinal;
+
+public class FirestoreRecyclerAdapter<T, T1> {
+    protected java.awt.image.VolatileImage getSnapshots() {
+    }
+}
---
+++ app/src/main/java/com/example/notesapptutorial/sections.java
@@ -0,0 +1,14 @@
+package com.example.notesapptutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sectionss extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sectionss);
+    }
+}
